f54aebad35dc6ecc8c547dbef7bd1356a9202e6e
Marc Kelechava <marcmuon@gmail.com>
2021-11-30 22:59:32 -0800

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val nums = input.map{it.toInt()}
+        var increasingCt = 0
+        var lastNum = nums[0]
+        for (n in nums) {
+            if (n > lastNum) {
+                increasingCt++
+            }
+            lastNum = n
+        }
+        return increasingCt
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val nums = input.map{it.toInt()}
+        var increaseCt = 0
+        var lastWindowSum = nums.slice(0 until 3).sum()
+        for (i in 0 until nums.size-2) {
+            val currWindowSum = nums.slice(i until i+3).sum()
+            if (currWindowSum > lastWindowSum) {
+                increaseCt++
+            }
+            lastWindowSum = currWindowSum
+        }
+        return increaseCt
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
