e416c541cf5a4ca92d46d801ec4ea00c26c87063
Marc Kelechava <marc.kelechava@gmail.com>
2021-12-22 16:21:11 -0800

    day1 jb version

---
+++ src/CleanDay.kt
@@ -0,0 +1,16 @@
+fun main() {
+    fun part1() {
+        //TODO
+    }
+    fun part2() {
+        //TODO
+    }
+
+//    val testInput = readInput("Day03_test")
+//    check(part1(testInput) == 0)
+//    check(part2(testInput) == 0)
+//
+//    val input = readInput("Day03")
+//    println(part1(input))
+//    println(part2(input))
+}
